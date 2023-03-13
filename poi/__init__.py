e517119103dd69eaf389b65142fd0b63f9e5527d
Ryan Wang <hwwangwang@gmail.com>
2023-03-13 15:10:30 +0800

    export

--- poi/__init__.py
+++ poi/__init__.py
@@ -4,5 +4,7 @@ __version__ = "1.0.0"
 from .sheet import Sheet
 from .book import Book
 from .nodes import Cell, Box, Col, Row, Table
+from .writer import BytesIOWorkBook
 
-__all__ = ["Sheet", "Book", "Cell", "Box", "Col", "Row", "Table"]
+
+__all__ = ["Sheet", "Book", "Cell", "Box", "Col", "Row", "Table", "BytesIOWorkBook"]
